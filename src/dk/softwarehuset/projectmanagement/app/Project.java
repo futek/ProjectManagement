0417fe6bf0b57f6ed597617aa5c17ebfb3fb9792
Jamie Neubert Pedersen <zajaka@hotmail.com>
2012-04-12 14:49:20 +0200

    Add test for join and leave project, and implemented.. and stuff

--- src/dk/softwarehuset/projectmanagement/app/Project.java
+++ src/dk/softwarehuset/projectmanagement/app/Project.java
@@ -1,8 +1,12 @@
 package dk.softwarehuset.projectmanagement.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Project {
 	private String id;
 	private String name;
+	private List<Employee> employees = new ArrayList<Employee>();
 
 	public Project(String id, String name) {
 		this.id = id;
@@ -19,5 +23,23 @@ public class Project {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void addEmployee(Employee newEmployee) throws NonUniqueIdentifierException {
+		for (Employee employee : employees) {
+			if (id.equals(employee.getId())) {
+				throw new NonUniqueIdentifierException("Employee on project already");
+			}
+		}
+		
+		employees.add(newEmployee);
+	}
+
+	public void removeEmployee(Employee employee) {
+		employees.remove(employee);
 	}
 }
---
+++ test/dk/softwarehuset/projectmanagement/app/TestJoinAndLeaveProject.java
@@ -0,0 +1,51 @@
+package dk.softwarehuset.projectmanagement.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dk.softwarehuset.projectmanagement.util.SampleDataSetupWithProjects;
+
+public class TestJoinAndLeaveProject extends SampleDataSetupWithProjects {
+	
+	@Test
+	public void testJoinProject() throws NonUniqueIdentifierException {
+		String id = "120001";
+		// check no employees on project
+		assertEquals(0, app.getProjectById(id).getEmployees().size());
+		
+		Employee employee = app.getEmployeeById("ABCD");
+		Project project = app.getProjectById(id);
+		
+		// add employee to project
+		project.addEmployee(employee);
+		
+		// check employee is on project
+		assertEquals(1, app.getProjectById(id).getEmployees().size());
+		assertEquals(employee, app.getProjectById(id).getEmployees().get(0));
+	}
+	
+	@Test
+	public void testLeaveProject() throws NonUniqueIdentifierException {
+		String id = "120001";
+		// check no employees on project
+		assertEquals(0, app.getProjectById(id).getEmployees().size());
+		
+		Employee employee = app.getEmployeeById("ABCD");
+		Project project = app.getProjectById(id);
+		
+		// add employee to project
+		project.addEmployee(employee);
+		
+		// check employee is on project
+		assertEquals(1, app.getProjectById(id).getEmployees().size());
+		assertEquals(employee, app.getProjectById(id).getEmployees().get(0));
+		
+		// remove employee from project
+		project.removeEmployee(employee);
+		
+		// check no employee is on the project
+		assertEquals(0, app.getProjectById(id).getEmployees().size());
+	}
+
+}
