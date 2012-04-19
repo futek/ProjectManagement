87b79bcf0515438655e22bbe8daba5e72886cb16
Salvador Jacobi <salvador.jacobi@gmail.com>
2012-04-19 13:27:00 +0200

    add create activity app test

---
+++ test/dk/softwarehuset/projectmanagement/app/TestCreateActivity.java
@@ -0,0 +1,39 @@
+package dk.softwarehuset.projectmanagement.app;
+
+import org.junit.Test;
+
+import dk.softwarehuset.projectmanagement.util.SampleDataSetupWithProjects;
+
+public class TestCreateActivity extends SampleDataSetupWithProjects {
+	@Test
+	public void testCreateActivityOnProject() {
+		Project project = app.getProjectById("120001");
+		String name = "Design phase";
+
+		// Check no activies exist on project
+		assertEquals(0, project.getActivities().size());
+
+		// Create activity on project
+		Activtiy activity = app.createActivity(project, name);
+
+		// Check activity exists on project
+		assertEquals(1, project.getActivities().size());
+		assertEquals(activities, project.getActivities().get(0));
+	}
+
+	@Test
+	public void testCreateActivityOnEmployee() {
+		Employee employee = app.getEmployeeById("ABCD");
+		String name = "Some course";
+
+		// Check no activies exist on employee
+		assertEquals(0, employee.getActivities().size());
+
+		// Create activity on employee
+		Activtiy activity = app.createActivity(employee, name);
+
+		// Check activity exists on project
+		assertEquals(1, employee.getActivities().size());
+		assertEquals(activity, employee.getActivities().get(0));
+	}
+}
