98baff87b2a07610ce08d7f587cbee010828bd4d
Salvador Jacobi <salvador.jacobi@gmail.com>
2012-04-23 08:22:45 +0200

    organize imports

--- test/dk/softwarehuset/projectmanagement/ui/TestEditProjectProperties.java
+++ test/dk/softwarehuset/projectmanagement/ui/TestEditProjectProperties.java
@@ -1,7 +1,5 @@
 package dk.softwarehuset.projectmanagement.ui;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 
 import org.junit.Test;
@@ -10,10 +8,10 @@ import dk.softwarehuset.projectmanagement.app.WrongCredentialsException;
 import dk.softwarehuset.projectmanagement.util.ApplicationUITester;
 import dk.softwarehuset.projectmanagement.util.SampleDataSetupWithProjects;
 
-public class TestEditProjectProperties extends SampleDataSetupWithProjects{
+public class TestEditProjectProperties extends SampleDataSetupWithProjects {
 	private ApplicationUI appUI = new ApplicationUI(app);
 	private ApplicationUITester appUITester = new ApplicationUITester(appUI);
-	
+
 	@Test
 	public void testEditProject() throws WrongCredentialsException, IOException {
 		// Sign in as an employee
@@ -28,35 +26,32 @@ public class TestEditProjectProperties extends SampleDataSetupWithProjects{
 
 		// Register project leader
 		appUITester.selectOption("Register as project leader").expect("You're now project leader for the project \"Good afternoon World!\".");
-		
+
 		// Enter edit project properties screen
 		appUITester.selectOption("Edit project properties").expectNothing();
-		
+
 		// Edit name
 		appUITester.selectOption("Edit name").expectNothing();
 		appUITester.expect("Old project name: Good afternoon World!", "New project name: ").write("libaro").expectNothing();
-		
+
 		// Edit start date
 		appUITester.selectOption("Edit start date").expectNothing();
 		appUITester.expect("New start year: ").write("2012").expectNothing();
 		appUITester.expect("New start week: ").write("5").expectNothing();
-		
+
 		// Edit end date
 		appUITester.selectOption("Edit end date").expectNothing();
 		appUITester.expect("New end year: ").write("2012").expectNothing();
 		appUITester.expect("New end week: ").write("6").expectNothing();
-		
-		
+
 		// Fail on setting start date. After end date
 		appUITester.selectOption("Edit start date").expectNothing();
 		appUITester.expect("Old start year: 2012", "New start year: ").write("2012").expectNothing();
 		appUITester.expect("Old start week: 5", "New start week: ").write("7").expect("Start date after end date. Try again.");
-		
+
 		// Fail on setting end date. Before start date
 		appUITester.selectOption("Edit end date").expectNothing();
 		appUITester.expect("Old end year: 2012", "New end year: ").write("2012").expectNothing();
 		appUITester.expect("Old end week: 6", "New end week: ").write("4").expect("End date before start date. Try again.");
-		
 	}
-
 }
